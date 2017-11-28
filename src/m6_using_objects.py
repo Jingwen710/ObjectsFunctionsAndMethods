97240f8f73e7f271e16b85a0b0c82484ff907bd9
wuj1 <wuj1@rose-hulman.edu>
2017-11-27 20:26:16 -0500

    I drew lines, circles and rectangles.

--- src/m6_using_objects.py
+++ src/m6_using_objects.py
@@ -5,8 +5,8 @@ This module lets you practice  ** using objects **, including:
   -- accessing their DATA via INSTANCE VARIABLES
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jingwen Wu.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -14,6 +14,9 @@ import rosegraphics as rg
 def main():
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
+    two_circles()
+    circle_and_rectangle()
+    lines()
 
 
 def two_circles():
@@ -27,12 +30,28 @@ def two_circles():
     -- Waits for the user to press the mouse, then closes the window.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function, per its doc-string above.
+    # Done: 2. Implement this function, per its doc-string above.
     #    -- ANY two rg.Circle objects that meet the criteria are fine.
     #    -- File  COLORS.txt  lists all legal color-names.
     # Put a statement in   main   to test this function
     #    (by calling this function).
     # ------------------------------------------------------------------
+    window = rg.RoseWindow()
+
+    center_point = rg.Point(100, 200)
+    radius = 25
+    circle = rg.Circle(center_point, radius)
+    circle.fill_color = 'blue'
+    circle.attach_to(window)
+
+    center_point_new = rg.Point(200, 250)
+    radius_new = 10
+    circle = rg.Circle(center_point_new, radius_new)
+    circle.attach_to(window)
+
+    window.render()
+
+    window.close_on_mouse_click()
 
 
 def circle_and_rectangle():
@@ -67,7 +86,7 @@ def circle_and_rectangle():
            150.0
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement this function, per its doc-string above.
+    # Done: 3. Implement this function, per its doc-string above.
     #   -- ANY objects that meet the criteria are fine.
     # Put a statement in   main   to test this function
     #    (by calling this function).
@@ -75,6 +94,37 @@ def circle_and_rectangle():
     # IMPORTANT: Use the DOT TRICK to guess the names of the relevant
     #       instance variables for outline thickness, etc.
     # ------------------------------------------------------------------
+    window = rg.RoseWindow()
+
+    point_x = 200
+    point_y = 200
+    center_point = rg.Point(point_x, point_y)
+    radius = 40
+    circle = rg.Circle(center_point, radius)
+    circle.outline_thickness = 5
+    circle.fill_color = 'blue'
+    circle.attach_to(window)
+
+    point1 = rg.Point(10, 10)
+    point2 = rg.Point(100, 100)
+    rectangle = rg.Rectangle(point1, point2)
+    rectangle.outline_thickness = 10
+    rectangle.attach_to(window)
+    center = rectangle.get_center()
+    window.render()
+
+    window.close_on_mouse_click()
+
+    print(circle.outline_thickness)
+    print(circle.fill_color)
+    print(center_point)
+    print(point_x)
+    print(point_y)
+    print(rectangle.outline_thickness)
+    print(rectangle.fill_color)
+    print(center)
+    print(center.x)
+    print(center.y)
 
 
 def lines():
@@ -100,8 +150,28 @@ def lines():
     -- Waits for the user to press the mouse, then closes the window.
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     # ------------------------------------------------------------------
+    window = rg.RoseWindow()
+
+    start_point = rg.Point(100, 100)
+    end_point = rg.Point(200, 200)
+    line = rg.Line(start_point, end_point)
+    line.thickness = 20
+    line.attach_to(window)
+    center = line.get_midpoint()
+
+    start_new = rg.Point(150, 150)
+    end_new = rg.Point(200, 100)
+    line = rg.Line(start_new, end_new)
+    line.attach_to(window)
+
+    window.render()
+    window.close_on_mouse_click()
+
+    print(center)
+    print(center.x)
+    print(center.y)
 
 
 # ----------------------------------------------------------------------
